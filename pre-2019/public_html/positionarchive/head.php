845c0444a688ba2e871a4492ff59190edcc51b06
Mara Hart <maralihart@gmail.com>
2019-08-07 15:14:53 -0700

    Deleted old site

--- pre-2019/public_html/positionarchive/head.php
+++
@@ -1,9 +0,0 @@
-		
-		<link rel="icon" href="images/sun.png">
-		
-		<title>Organization of Young Filipino Americans</title>
-		<meta charset="utf-8" />
-		<meta name="viewport" content="width=device-width, initial-scale=1" />
-		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
-		<link rel="stylesheet" href="assets/css/main.css" />
-		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
