9270dd720d0b626b422652afa08e85728c05e568
Mara Hart <maralihart@gmail.com>
2020-02-20 10:07:56 -0500

    Updated officers page and added a couple bios

---
+++ images/positionarchive.php
@@ -0,0 +1,148 @@
+<!DOCTYPE HTML>
+<!--
+	Helios by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<?php include("head.php");?>
+	</head>
+	<body class="no-sidebar">
+		<div id="page-wrapper">
+
+			<!-- Header -->
+				<div id="header">
+
+					<?php include("header.php");?>
+				
+				</div>
+
+		<!-- Main -->
+		<div class="wrapper style1">
+
+			<div class="container">
+				<article id ="main">
+					<div align= "center"><img class="img-responsive" src="images/template/position.png" width="100%">
+					</div>
+					<div>
+						<header>
+							<h1>Board and Council</h1>
+						</header>
+					</div>
+				</article>
+				</br>
+				</br>
+				<div class="row 200%">
+					<div class="4u 12u(mobile)" id="sidebar">
+								<a href="#" class="image featured"><img src="images/2017bc.jpg" alt="" /></a>
+					</div>
+					<div class= "8u 12u(mobile)" id ="content">
+						<article>
+							<header>
+								<h2><a href="2017council.php">2017-2018</a></h2>
+							</header>
+							<h4><strong>President</strong><a href="2017president.php">: Joe Malasa</a></h4>
+							<h4><strong>Vice President</strong><a href="2017vp.php">: Jerome Tirso</a></h4>
+							<h4><strong>Treasurer</strong><a href="2017treasurer.php">: Jane Canteros</a></h4>
+							<h4><strong>Corresponding Secretary</strong><a href="2017corsec.php">: Alexa Gavino</a></h4>
+							<h4><strong>Recording Secretary</strong><a href="2017recsec.php">: Jaden Aponte</a></h4>
+							<h4><strong>Advocacy Chairs</strong><a href="2017advocacy.php">: Thyra Jensen and Dorian Nguyen</a></h4>
+							<h4><strong>Community Chairs</strong><a href="2017community.php">: Gabby Garvey and Arielle Reyes</a></h4>
+							<h4><strong>Culture Chairs</strong><a href="2017culture.php">: Roseanna Quinanola and Sophia Williams</a></h4>
+							<h4><strong>FIND D7 Representatives</strong><a href="2017d7.php">: Chelsea Bengson and John Ramirez</a></h4>
+							<h4><strong>Historic Chairs</strong><a href="2017historic.php">: Jack Chang and Pia Mutia</a></h4>
+							<h4><strong>Membership Chairs</strong><a href="2017membership.php">: Dana Kim and Michael Ramirez</a></h4>
+							<h4><strong>Public Relations Chairs</strong><a href="2017pr.php">: Baohan Do and Hayden Williams</a></h4>
+							<h4><strong>Scholarship Chairs</strong><a href="2017scholarship.php">: Angel Jomuad and Rose Montgomery</a></h4>
+							<h4><strong>Social Chairs</strong><a href="2017social.php">: Jessica Phan and Sophia Xiao</a></h4>
+							<h4><strong>Sports Chairs</strong><a href="2017sports.php">: Sarah Lei and Brandon Phan</a></h4>
+							<h4><strong>Student Advisors</strong><a href="2017advisors.php">: Richard Dizon and Danielle Rodgers</a></h4>
+							<h4><strong>Webmaster</strong><a href="2017webmaster.php">: Regina Yap</a></h4>
+							
+						</article>
+					</div> 
+				</div>
+				<div class="row 200%">
+					<div class="4u 12u(mobile)" id="sidebar">
+								<a href="#" class="image featured"><img src="images/2017bc.jpg" alt="" /></a>
+					</div>
+					<div class= "8u 12u(mobile)" id ="content">
+						<article>
+							<header>
+								<h2><a href="positionarchive/2016council.php">2016-2017</a></h2>
+							</header>
+							<h4><strong>President</strong><a href="positionarchive/2016president.php">: Madelyn Luansing</a></h4>
+							<h4><strong>Vice President</strong><a href="positionarchive/2016vp.php">: Alex Zarzuela</a></h4>
+							<h4><strong>Treasurer</strong><a href="positionarchive/2016treasurer.php">: Jerome Tirso</a></h4>
+							<h4><strong>Corresponding Secretary</strong><a href="positionarchive/2016corsec.php">: Kanata Omori</a></h4>
+							<h4><strong>Recording Secretary</strong><a href="positionarchive/2016recsec.php">: Christine Le</a></h4>
+							<h4><strong>Advocacy Chairs</strong><a href="positionarchive/2016advocacy.php">: Oanh Dao and Melissa Lim</a></h4>
+							<h4><strong>Community Chairs</strong><a href="positionarchive/2016community.php">: Celine Nguyen and Oliver Viyar</a></h4>
+							<h4><strong>Culture Chairs</strong><a href="positionarchive/2016culture.php">: Steven Ling</a></h4>
+							<h4><strong>FIND D7 Representatives</strong><a href="positionarchive/2016d7.php">: John Herrick and Danielle Rodgers</a></h4>
+							<h4><strong>Historic Chairs</strong><a href="positionarchive/2016historic.php">: Aaron Aguhob and Larry Han</a></h4>
+							<h4><strong>Membership Chairs</strong><a href="positionarchive/2016membership.php">: Joe Malasa and Mark Mora</a></h4>
+							<h4><strong>Public Relations Chairs</strong><a href="positionarchive/2016pr.php">: Jasmin Nguyen and Ky Nguyen</a></h4>
+							<h4><strong>Scholarship Chairs</strong><a href="positionarchive/2016scholarship.php">: Abigail Hoang and Caroline Pham</a></h4>
+							<h4><strong>Social Chairs</strong><a href="positionarchive/2016social.php">: Barry Chin and Jack Chang</a></h4>
+							<h4><strong>Sports Chairs</strong><a href="positionarchive/2016sports.php">: Alexa Gavino and Ronald Diu</a></h4>
+							<h4><strong>Student Advisors</strong><a href="positionarchive/2016advisors.php">: Vince Egalla and Elexa Rallos</a></h4>
+							<h4><strong>Webmaster</strong><a href="positionarchive/2016webmaster.php">: Chris Fassoth</a></h4>
+							
+						</article>
+					</div> 
+				</div>
+				<div class="row 200%">
+					<div class="4u 12u(mobile)" id="sidebar">
+								<a href="#" class="image featured"><img src="images/2017bc.jpg" alt="" /></a>
+					</div>
+					<div class= "8u 12u(mobile)" id ="content">
+						<article>
+							<header>
+								<h2><a href="2017council.php">2017-2018</a></h2>
+							</header>
+							<h4><strong>President</strong><a href="2017president.php">: Joe Malasa</a></h4>
+							<h4><strong>Vice President</strong><a href="2017vp.php">: Jerome Tirso</a></h4>
+							<h4><strong>Treasurer</strong><a href="2017treasurer.php">: Jane Canteros</a></h4>
+							<h4><strong>Corresponding Secretary</strong><a href="2017corsec.php">: Alexa Gavino</a></h4>
+							<h4><strong>Recording Secretary</strong><a href="2017recsec.php">: Jaden Aponte</a></h4>
+							<h4><strong>Advocacy Chairs</strong><a href="2017advocacy.php">: Thyra Jensen and Dorian Nguyen</a></h4>
+							<h4><strong>Community Chairs</strong><a href="2017community.php">: Gabby Garvey and Arielle Reyes</a></h4>
+							<h4><strong>Culture Chairs</strong><a href="2017culture.php">: Roseanna Quinanola and Sophia Williams</a></h4>
+							<h4><strong>FIND D7 Representatives</strong><a href="2017d7.php">: Chelsea Bengson and John Ramirez</a></h4>
+							<h4><strong>Historic Chairs</strong><a href="2017historic.php">: Jack Chang and Pia Mutia</a></h4>
+							<h4><strong>Membership Chairs</strong><a href="2017membership.php">: Dana Kim and Michael Ramirez</a></h4>
+							<h4><strong>Public Relations Chairs</strong><a href="2017pr.php">: Baohan Do and Hayden Williams</a></h4>
+							<h4><strong>Social Chairs</strong><a href="2017social.php">: Jessica Phan and Sophia Xiao</a></h4>
+							<h4><strong>Sports Chairs</strong><a href="2017sports.php">: Sarah Lei and Brandon Phan</a></h4>
+							<h4><strong>Student Advisors</strong><a href="2017advisors.php">: Richard Dizon and Danielle Rodgers</a></h4>
+							<h4><strong>Webmaster</strong><a href="2017webmaster.php">: Regina Yap</a></h4>
+							
+						</article>
+					</div> 
+				</div>
+					  
+			</div>
+
+		</div>
+
+			<!-- Footer -->
+				<div id="footer">
+				<?php include("footer.php")?>					
+				</div>
+
+	</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.dropotron.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/jquery.onvisible.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
